446a28a6713ec97a8d9dc64ef8cb07ce642e46c8
Rafael Kepler <rafael.kepler@hotmail.com>
2022-09-13 00:18:23 -0300

    fixes

--- App.tsx
+++ App.tsx
@@ -1,20 +1,33 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import { StyleSheet, Text, TouchableOpacity, View } from 'react-native';
+
+interface ButtonProps {
+  title: string;
+}
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Open up App.tsx to start working on your app!</Text>
+      <Text>Hello world</Text>
+      <Button title="teste" />
       <StatusBar style="auto" />
     </View>
   );
 }
 
+function Button(props: ButtonProps) {
+  return (
+    <TouchableOpacity>
+      <Text>{props.title}</Text>
+    </TouchableOpacity>
+  )
+}
+
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#FFF',
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
